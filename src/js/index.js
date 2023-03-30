f0d8956c248c8c3778dd7af46d10da5f15d773d5
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-03-30 10:43:01 -0300

    add new section

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const card = document.querySelector(".card");
+const sectioNature = document.querySelector(".nature")
+
+sectioNature.addEventListener("click", ( e ) => {
+  const getEvent = e.currentTarget;
+  
+  console.log( getEvent)
+})
+

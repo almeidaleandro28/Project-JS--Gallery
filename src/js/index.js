f1f63381aaa8ee821ed11c8c0eb9ab9b7490d2ce
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-04-10 11:26:19 -0300

    add modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 const card = document.querySelector(".card");
 const sectioNature = document.querySelector(".nature")
+const sectioCity = document.querySelector(".city");
+const main = document.querySelector(".main");
+const modal = document.querySelector(".modal");
+
 
 sectioNature.addEventListener("click", ( e ) => {
-  const getEvent = e.currentTarget;
-  
-  console.log( getEvent)
+  // const getEvent = e.currentTarget;
+  sectioCity.style.display = "none";
+  sectioNature.style.display = "none";
+  modal.style.display = "block";
 })
 

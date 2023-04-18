90baec92be91cfc4087b748c18fc335bdc51ac0d
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-04-18 08:58:59 -0300

    feet - add event
    Event add for section city

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,15 @@ const main = document.querySelector(".main");
 const modal = document.querySelector(".modal");
 const btClose = document.querySelector(".btn_close");
 
+
 sectioNature.addEventListener("click", ( e ) => {
   // const getEvent = e.currentTarget;
+  sectioCity.display = "none";
+  sectioNature.display = "none";
+  modal.style.display = "block";
+});
+
+sectioCity.addEventListener("click", ( e ) => {
   sectioCity.display = "none";
   sectioNature.display = "none";
   modal.style.display = "block";
@@ -15,4 +22,6 @@ sectioNature.addEventListener("click", ( e ) => {
 btClose.addEventListener("click", () => {
   modal.style.display = "none";
 });
+
+
 

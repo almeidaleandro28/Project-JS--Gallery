18de898cd31f2fbdb071e52a5e0d9331618e9767
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-04-28 14:08:24 -0300

    add img
    add all img of section nature inside footer gallery

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,10 @@ const main = document.querySelector(".main");
 const modal = document.querySelector(".modal");
 const btClose = document.querySelector(".btn_close");
 let $modalCenter = document.querySelector(".modal_body_center");
-const $natureImgs = document.querySelector(".nature_img");
+let $modalFooter = document.querySelector(".modal_footer");
+let $modalFooterGallery = document.querySelector(".modal_footer_gallery");
+const $natureImgs = document.querySelectorAll(".nature_img");
+
 
 
 
@@ -18,8 +21,15 @@ sectioNature.addEventListener("click", ( e ) => {
 
   let currentImg = e.target;
   let imgSrc = currentImg.src;
+  let imgTitle = currentImg.title;
 
-  $modalCenter.innerHTML = `<img src=${imgSrc} />`;
+  $modalCenter.innerHTML = `<img src=${imgSrc} />
+                            <h1 class="h1">${imgTitle}<h1>`;
+
+  $natureImgs.forEach( img => {
+    $modalFooterGallery.innerHTML += `<img src=${img.src}>`;
+  });
+
 
 });
 
